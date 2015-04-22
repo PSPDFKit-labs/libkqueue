35640fcade87263edd9ace6ffd8826415e405dc3
Patrik Weiskircher <patrik@weiskircher.name>
2015-04-22 11:15:50 -0400

    Added Android.mk

---
+++ Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE		:= libkqueue
+LOCAL_C_INCLUDES	:= $(LOCAL_PATH)/src/common \
+					   $(LOCAL_PATH)/android \
+					   $(LOCAL_PATH)/include
+LOCAL_CFLAGS		:= -std=c99 -D_XOPEN_SOURCE=600 -fvisibility=hidden
+LOCAL_SRC_FILES		:=  \
+	src/common/filter.c \
+    src/common/knote.c \
+	src/common/map.c \
+	src/common/kevent.c \
+	src/common/kqueue.c \
+	src/posix/platform.c \
+	src/linux/platform.c \
+	src/linux/read.c \
+	src/linux/write.c \
+	src/linux/user.c \
+	src/linux/vnode.c \
+	src/linux/timer.c \
+	src/posix/signal.c
+
+include $(BUILD_STATIC_LIBRARY)
+
